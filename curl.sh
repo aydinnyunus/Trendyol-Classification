6b609b8545f378243a106bf0ac3f64ecac897091
Yunus AYDIN <yunusaydin@strixeye.com>
2022-10-26 13:09:32 +0300

    add color

--- curl.sh
+++ curl.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 set -B                  # enable brace expansion
+
+# COLORS
+
+## Reset
+NC='\033[0m'       # Text Reset
+
+## Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+
 categories=("erkek-t-shirt-x-g2-c73" "erkek-sort-x-g2-c119" "erkek-gomlek-x-g2-c75" "erkek-esofman-x-g2-c1049" "erkek-pantolon-x-g2-c70" "erkek-ceket-x-g2-c1030" "erkek-sweatshirt-x-g2-c1179" "erkek-yelek-x-g2-c1207" "erkek-kazak-x-g2-c1092" "erkek-mont-x-g2-c118")
 
 folders=("tisort" "sort" "gomlek" "esofman" "pantolon" "ceket" "sweat" "yelek" "kazak" "mont")
@@ -8,10 +25,10 @@ mkdir -p tisort sort gomlek esofman pantolon ceket sweat yelek kazak mont
 
 j=0
 for str in ${categories[@]}; do
-  echo $str
+  echo -e "${Purple}[*] $str is analyzing${NC}" 
   for i in {1..10}; do
     
-    curl 'https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/'$str'?pi='$i'&culture=tr-TR&userGenderId=1&pId=0&scoringAlgorithmId=2&categoryRelevancyEnabled=false&isLegalRequirementConfirmed=false&searchStrategyType=DEFAULT&productStampType=TypeA&fixSlotProductAdsIncluded=true&offset='$i*16 \
+    curl -s 'https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/'$str'?pi='$i'&culture=tr-TR&userGenderId=1&pId=0&scoringAlgorithmId=2&categoryRelevancyEnabled=false&isLegalRequirementConfirmed=false&searchStrategyType=DEFAULT&productStampType=TypeA&fixSlotProductAdsIncluded=true&offset='$i*16 \
       -H 'authority: public.trendyol.com' \
       -H 'accept: application/json, text/plain, */*' \
       -H 'accept-language: tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7' \
@@ -27,8 +44,10 @@ for str in ${categories[@]}; do
     cat ./*_last.txt > last.txt
 
   done
-  wget -i last.txt
+  echo -e "${Cyan}[*] Images are downloading${NC}"
+  wget -q -i last.txt --show-progress
   mv last.txt *.jpg *.jpg* ${folders[$j]}
   rm -rf *.txt
+  echo -e "${Cyan}[*] $str is finished${NC}"
   j=$j+1
 done
